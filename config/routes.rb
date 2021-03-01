3d7d24307c81ccf1d00172fee909c0eb04af640f
EC2 Default User <ec2-user@ip-172-31-94-195.ec2.internal>
2021-03-01 08:09:09 +0000

    [Finish]応用課題1

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'homes#top'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
   resources :books
 end
